2dd84bb05bcf7e7723bb3a8df80090ad0e1c44eb
MathibelaD <dineomathibela161@gmail.com>
2023-04-03 16:33:49 +0200

    ????

--- m5.js
+++ m5.js
@@ -1,22 +1,39 @@
 const url = "http://localhost:4377/";
-// const reQ = new Promise(rej);
-const fetchData = async (url) => {
-  const req = new XMLHttpRequest();
-  try {
-    req.open("GET", url); // open channel
-    req.send(); // send request
-    const resp = await req.response;
-    console.log(resp)
-    console.log(req);
-    return resp;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-};
 
-fetchData(url)
+// const fetchData = async (url) => {
+//   const req = new XMLHttpRequest();
+//   try {
+//     req.open("GET", url); // open channel
+//     req.send(); // send request
+//     const resp = await req.response;
+//     return resp;
+//   } catch (error) {
+//     console.log(error);
+//     return error;
+//   }
+// };
+
+function load(url, callback) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      callback(xhr.response);
+      console.log(xhr.response);
+    }
+  }
+
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+load(url, () => {})
+// await fetchData(url)
 //   .then((data) => {
 //     console.log(data);
 //   })
 //   .catch((error) => console.log(error));
+//========================================
+ let lesson_data = document.getElementById("lessons")
+
+//  lesson_data.innerText = fetchData(url)

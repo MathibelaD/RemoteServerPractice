695f6d4cf524dcc9b695e95c03b9cbafc2ac725e
MathibelaD <dineomathibela161@gmail.com>
2023-04-04 02:17:53 +0200

    dispalys data

--- m5.js
+++ m5.js
@@ -1,13 +1,13 @@
 const url = "http://localhost:4377/";
-const getData = document.getElementById("lessons")
+// const getData = document.getElementById("lessons")
 
-const manipulate = (test) => {
-  return getData.innerText = test
-}
+// const manipulate = (test) => {
+//   return getData = test
+// }
 
 const fetchData = async () => {
   const req = new XMLHttpRequest();
-  req.open("GET", "http://localhost:4377/");
+  req.open("GET", "http://localhost:4377/", true);
 
   try {
     req.send();
@@ -16,9 +16,8 @@ const fetchData = async () => {
     if(req.readyState === 4){
       console.log(req.response)
       let data = req.response
-      // manipulate("data hehehehe")
-    }else {
-      console.log("Loading", req.readyState)
+      document.getElementById("lessons").innerText = req.response
+      return data
     }
   }
   } catch (error) {
@@ -28,21 +27,3 @@ const fetchData = async () => {
 };
 
 fetchData()
-// function load(url, callback) {
-//   const xhr = new XMLHttpRequest();
-
-//   xhr.onreadystatechange = () => {
-//     if (xhr.readyState === 4) {
-//       return callback(JSON.parse(xhr.response));
-//       //  console.log(JSON.parse(xhr.response));
-//     }
-//   }
-//   xhr.open('GET', url, true);
-//   xhr.send({message: "a ke sure"});
-// }
-
-// console.log(load(url, () => {}))
-
-//  let lesson_data = document.getElementById("lessons")
-
-// //  lesson_data.innerText = load(url, () => {})

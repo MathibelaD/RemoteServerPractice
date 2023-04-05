ceae0e8a6ad1394b87f239a1fde802b4bc00deac
MathibelaD <dineomathibela161@gmail.com>
2023-04-05 06:29:41 +0200

    svg circle

--- m5.js
+++ m5.js
@@ -34,12 +34,19 @@ const fetchData = async () => {
 
 // create circular svg image  with red background and purple boder. add  text to the image 'I am SVG' centered.
 const svgholder = document.getElementById("imageX")
-// const svg = document.createElement('svg')
+// const svg = document.createElementNS("http://www.w3.org/2000/svg",'svg')
 // svgholder.appendChild(
-//   Object.assign(document.createElement('svg'),{
+//   Object.assign(document.createElementNS("http://www.w3.org/2000/svg",'svg'),{
 //     height : "100",
 //     width: "100"
-//   }))
+//   })).appendChild(
+//     Object.assign(document.createElementNS("http://www.w3.org/2000/svg",'circle'),{
+//       cx: "50",
+//       cy: "50",
+//       r: "40"
+//     })
+//   )
+
 const svg1 = document.createElementNS("http://www.w3.org/2000/svg",'svg')
 svg1.setAttribute('width', '100')
 svg1.setAttribute('height', '100')  
@@ -48,7 +55,8 @@ const circle1 = document.createElementNS("http://www.w3.org/2000/svg",'circle')
 circle1.setAttribute("cx",50)
 circle1.setAttribute("cy",50)
 circle1.setAttribute("r",40)
-// circle1.setAttribute("style",20)
+circle1.setAttribute('style', 'fill: red; stroke: purple; stroke-width: 4px;')
+// circle1.setAttribute("stroke",red)
 svg1.appendChild(circle1)
-document.body.appendChild(svg1)
+svgholder.appendChild(svg1)
 fetchData()

b7b75b2c6d995e08180692569ed74babad74a31f
MathibelaD <dineomathibela161@gmail.com>
2023-04-11 10:35:26 +0200

    worker file init

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 const app = express();
 const port = 4377;
-app.use(express.json());
+app.use(express.json());// ?
 app.use(cors({ origin: "*" }));
 app.get("/", (req, resp) => {
   resp.status(200).send({ message: "", lessons });
